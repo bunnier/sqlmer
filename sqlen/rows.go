12b4ace6d08f01fa8a1d4a178e4661ad9b8c5e08
bunnier <zrui@outlook.com>
2022-04-09 22:16:39 +0800

    完善Rows获取行元数据的逻辑，第一次获取ScanType后会缓存结果。

--- sqlen/rows.go
+++ sqlen/rows.go
@@ -17,18 +17,31 @@ type EnhanceRows struct {
 	getScanTypeFn GetScanTypeFunc // 用于获取用于 Scan 的数据类型。
 	unifyDataType UnifyDataTypeFn
 
-	err error
+	columnMetaSlice []*columnMeta // 用于对列的元数据做缓存。
 
-	columns  []string
-	colTypes []*sql.ColumnType
+	err error
+}
+
+type columnMeta struct {
+	colType  *sql.ColumnType // 列类型。
+	scanType reflect.Type    // 用于 Scan 的类型。
 }
 
 // 初始化查询列的元数据。
-func (rs *EnhanceRows) initColumns() {
-	if rs.columns == nil {
-		rs.columns, rs.err = rs.Columns()
-		rs.colTypes, rs.err = rs.ColumnTypes()
+func (rs *EnhanceRows) initColumns() error {
+	if rs.columnMetaSlice != nil {
+		return nil
 	}
+
+	if colTypes, err := rs.ColumnTypes(); err != nil {
+		return err
+	} else {
+		rs.columnMetaSlice = make([]*columnMeta, 0, len(colTypes))
+		for _, cType := range colTypes {
+			rs.columnMetaSlice = append(rs.columnMetaSlice, &columnMeta{cType, nil})
+		}
+	}
+	return nil
 }
 
 // MapScan 用于把一行数据填充到 map 中。
@@ -37,9 +50,7 @@ func (rs *EnhanceRows) Scan(dest ...interface{}) error {
 		return rs.err
 	}
 
-	rs.initColumns()
-
-	// 用原生 row 的 Scan 方法获取数据。
+	// 直接用原生 row 的 Scan 方法获取数据。
 	return rs.Rows.Scan(dest...)
 }
 
@@ -50,8 +61,8 @@ func (rs *EnhanceRows) MapScan(dest map[string]interface{}) error {
 		return err
 	}
 
-	for i, column := range rs.columns {
-		dest[column] = values[i]
+	for i, columnMeta := range rs.columnMetaSlice {
+		dest[columnMeta.colType.Name()] = values[i]
 	}
 
 	return nil
@@ -66,24 +77,29 @@ func (rs *EnhanceRows) SliceScan() ([]interface{}, error) {
 	rs.initColumns()
 
 	// 用来存放 Scan 后返回的数据，db 库要求和查询的列完全一致，所以需要判断 columns 长度。
-	dest := make([]interface{}, len(rs.colTypes))
-	destRefVal := make([]reflect.Value, len(rs.colTypes))
-	for i, cType := range rs.colTypes {
-		refType := unifyScanType(rs.getScanTypeFn(cType), cType) // TODO: 这里可以做个缓存优化，不需要每次去获取类型。
-		refVal := reflect.New(refType)                           // 使用数据库驱动标记的类型来接收数据。
-		dest[i] = refVal.Interface()                             // 注意，这里传入的是指定值的指针。
-		destRefVal[i] = refVal                                   // 保存这个 Reflect.value 在后面用于解引用。
+	dest := make([]interface{}, len(rs.columnMetaSlice))
+	destRefVal := make([]reflect.Value, len(rs.columnMetaSlice))
+
+	for i, colMeta := range rs.columnMetaSlice {
+		if colMeta.scanType == nil {
+			// 第一次查询，需要获取 scan 的类型，获取后缓存到列元数据里。
+			colMeta.scanType = unifyScanType(rs.getScanTypeFn(colMeta.colType), colMeta.colType)
+		}
+
+		refVal := reflect.New(colMeta.scanType) // 使用数据库驱动标记的类型来接收数据。
+		dest[i] = refVal.Interface()            // 注意，这里传入的是指定值的指针。
+		destRefVal[i] = refVal                  // 保存这个 Reflect.value 在后面用于解引用。
 	}
 
 	rs.Scan(dest...)
 
-	for i := 0; i < len(rs.colTypes); i++ {
+	for i := 0; i < len(rs.columnMetaSlice); i++ {
 		// 之前为了能让 Scan 修改数据，保存的是指针，而返回上层时候只需要实际数据，因此进行解引用。
 		dest[i] = destRefVal[i].Elem().Interface()
 
-		extractNullableColumnValue(rs.colTypes[i], &dest[i]) // 进行统一的空值处理逻辑。
+		extractNullableColumnValue(rs.columnMetaSlice[i].colType, &dest[i]) // 进行统一的空值处理逻辑。
 		if dest[i] != nil {
-			rs.unifyDataType(rs.colTypes[i], &dest[i]) // 进行数据库定制的类型处理。
+			rs.unifyDataType(rs.columnMetaSlice[i].colType, &dest[i]) // 进行数据库定制的类型处理。
 		}
 	}
 
