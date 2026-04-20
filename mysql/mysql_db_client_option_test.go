db65fb56b9c04f1eb7f9989eb94a22a4a26a94eb
Rui Zhong <bunnier1987@qq.com>
2026-04-20 10:58:00 +0800

    简化设置缓存池的参数的名称。

--- mysql/mysql_db_client_option_test.go
+++ mysql/mysql_db_client_option_test.go
@@ -7,15 +7,15 @@ import (
 	"github.com/bunnier/sqlmer/mysql"
 )
 
-func Test_WithQuestionMarkNamedSqlCacheCapacity(t *testing.T) {
-	_, err := sqlmer.NewDbClientConfig(mysql.WithQuestionMarkNamedSqlCacheCapacity(8))
+func Test_WithSqlParserCacheCapacity(t *testing.T) {
+	_, err := sqlmer.NewDbClientConfig(mysql.WithSqlParserCacheCapacity(8))
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
-func Test_WithQuestionMarkNamedSqlCacheCapacity_invalid_capacity(t *testing.T) {
-	_, err := sqlmer.NewDbClientConfig(mysql.WithQuestionMarkNamedSqlCacheCapacity(0))
+func Test_WithSqlParserCacheCapacity_invalid_capacity(t *testing.T) {
+	_, err := sqlmer.NewDbClientConfig(mysql.WithSqlParserCacheCapacity(0))
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
